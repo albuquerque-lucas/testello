a062ed2b88410cf634b1c8466c3160612136f8b8
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-25 10:15:29 -0300

    Ajustando formatacao do testo de MigrationTest

--- tests/Feature/MigrationTest.php
+++ tests/Feature/MigrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Schema;
 
 class MigrationTest extends TestCase
 {
@@ -13,8 +14,11 @@ class MigrationTest extends TestCase
     public function test_customers_table_has_expected_columns()
     {
         $this->assertTrue(
-            \Schema::hasColumns('customers', [
-                'id', 'name', 'created_at', 'updated_at'
+            Schema::hasColumns('customers', [
+                'id',
+                'name',
+                'created_at',
+                'updated_at',
             ]),
             1
         );
@@ -23,8 +27,17 @@ class MigrationTest extends TestCase
     public function test_freight_tables_table_has_expected_columns()
     {
         $this->assertTrue(
-            \Schema::hasColumns('freight_tables', [
-                'id', 'customer_id', 'from_postcode', 'to_postcode', 'from_weight', 'to_weight', 'cost', 'branch_id', 'created_at', 'updated_at'
+            Schema::hasColumns('freight_tables', [
+                'id',
+                'customer_id',
+                'from_postcode',
+                'to_postcode',
+                'from_weight',
+                'to_weight',
+                'cost',
+                'branch_id',
+                'created_at',
+                'updated_at',
             ]),
             1
         );
