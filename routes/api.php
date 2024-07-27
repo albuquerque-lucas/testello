e0bf96279236e070125718d233c4da8df69c1ec4
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-27 10:13:07 -0300

    Adicionando rotas do CRUD no arquivo api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FreightTableController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/freight-tables', [FreightTableController::class, 'index']);
 Route::get('/freight-tables/{id}', [FreightTableController::class, 'show']);
 Route::post('/upload-freight-csv', [FreightTableController::class, 'uploadCSV']);
+
+Route::apiResource('customers', CustomerController::class);
