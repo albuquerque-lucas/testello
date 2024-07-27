f43b40b1b60638abb0521c6fc0209396fc7c5444
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-27 11:47:07 -0300

    Refatorando arquivo de rotas api.php

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,8 @@ use App\Http\Controllers\FreightTableController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\BranchController;
 
-Route::get('/freight-tables', [FreightTableController::class, 'index']);
-Route::get('/freight-tables/{id}', [FreightTableController::class, 'show']);
 Route::post('/upload-freight-csv', [FreightTableController::class, 'uploadCSV']);
 
+Route::apiResource('freight-tables', FreightTableController::class);
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('branches', BranchController::class);
