28847f253677e5a089581cc34e5aaf073e188e53
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-27 09:32:13 -0300

    Corrigindo e implementando testes para as rotas GET de FreightTableController

--- database/factories/FreightTableFactory.php
+++ database/factories/FreightTableFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\FreightTable;
+use App\Models\Branch;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FreightTableFactory extends Factory
@@ -12,8 +13,8 @@ class FreightTableFactory extends Factory
     public function definition()
     {
         return [
-            'branch_id' => $this->faker->numberBetween(1, 10),
-            'customer_id' => $this->faker->optional()->numberBetween(1, 10),
+            'branch_id' => Branch::factory(),
+            'customer_id' => null,
             'from_postcode' => $this->faker->postcode,
             'to_postcode' => $this->faker->postcode,
             'from_weight' => $this->faker->randomFloat(2, 0, 100),
--- tests/Feature/FreightTableControllerTest.php
+++ tests/Feature/FreightTableControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\FreightTable;
+use App\Models\Branch;
 
 class FreightTableControllerTest extends TestCase
 {
@@ -12,6 +13,7 @@ class FreightTableControllerTest extends TestCase
 
     public function test_it_can_fetch_all_freight_tables()
     {
+        Branch::factory()->count(10)->create();
         FreightTable::factory()->count(5)->create();
 
         $response = $this->getJson('/api/freight-tables');
@@ -22,6 +24,7 @@ class FreightTableControllerTest extends TestCase
 
     public function test_it_can_fetch_a_single_freight_table_by_id()
     {
+        Branch::factory()->count(10)->create();
         $freightTable = FreightTable::factory()->create();
 
         $response = $this->getJson("/api/freight-tables/{$freightTable->id}");
